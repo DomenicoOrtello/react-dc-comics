8b08965e9a3dc448f92424c3615fc57d5ba3879f
Ortello7 <domenicoortello00@gmail.com>
2025-01-31 15:07:01 +0100

    feat create folder data and file navBarData.js

--- src/assets/components/Header.jsx
+++ src/assets/components/Header.jsx
@@ -1,42 +1,19 @@
+import navBarData from "../data/navBarData"
 export default function Header () {
     return (
         <div className="container">
             <img className="logo-img" src="./img/dc-logo.png" alt="logo"></img>
             <nav className="nav-header">
                 <ul>
-                    <li>
-                        <a href="#">CHARACTERS</a>
-                    </li>
-                    <li>
-                        <a id="comics" href="#">COMICS</a>
-                        <hr className="cazzillo"></hr>    
-                    </li>
-                    <li>
-                        <a href="#">MOVIES</a>
-                    </li>
-                    <li>
-                        <a href="#">TV</a>
-                    </li>
-                    <li>
-                        <a href="#">GAMES</a>
-                    </li>
-                    <li>
-                        <a href="#">COLLECTIBLES</a>
-                    </li>
-                    <li>
-                        <a href="#">VIDEOS</a>
-                    </li>
-                    <li>
-                        <a href="#">FANS</a>
-                    </li>
-                    <li>
-                        <a href="#">NEWS</a>
-                    </li>
-                    <li>
-                        <a href="#">SHOP</a>
-                    </li>
+                    {navBarData.map((navBar) => {
+                        return (
+                            <li key={navBarData.id} className={navBar.current ? "fregnetto" : ""}>
+                                <a href={navBar.url}>{navBar.text}</a>
+                            </li>
+                        )
+                    })}
                 </ul>
-            </nav>
+            </nav>  
         </div>
     )      
 }
