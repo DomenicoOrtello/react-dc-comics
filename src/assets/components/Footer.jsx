72aef1a6ba6d84efe10b64a36402b279281c69c1
Ortello7 <domenicoortello00@gmail.com>
2025-01-31 01:09:44 +0100

    feat part 1 of repo

--- src/assets/components/Footer.jsx
+++ src/assets/components/Footer.jsx
@@ -1,3 +1,147 @@
-export default function Footer () {
-    return ("")
+export default function Footer() {
+    return (
+      <footer>
+        <div className="container">
+          <div className="container-block">
+            <div className="block">
+              <img src="./img/buy-comics-digital-comics.png" alt="" />
+              <a href="#">DIGITAL COMICS</a>
+            </div>
+            <div className="block">
+              <img src="./img/buy-comics-merchandise.png" alt="" />
+              <a href="#">DC MERCHANDISE</a>
+            </div>
+            <div className="block">
+              <img src="./img/buy-comics-subscriptions.png" alt="" />
+              <a href="#">SUBSCRIPTION</a>
+            </div>
+            <div className="block">
+              <img src="./img/buy-comics-shop-locator.png" alt="" />
+              <a href="#">COMIC SHOP LOCATOR</a>
+            </div>
+            <div className="block">
+              <img src="./img/buy-dc-power-visa.svg" alt="" />
+              <a href="#">DC POWER VISA</a>
+            </div>
+          </div>
+        </div>
+        <div className="container-main-footer">
+          <div className="container-footer">
+            <div className="first-ul">
+              <h3>DC COMICS</h3>
+              <ul>
+                <li>
+                  <a href="#">Characters</a>
+                </li>
+                <li>
+                  <a href="#">Comics</a>
+                </li>
+                <li>
+                  <a href="#">Movies</a>
+                </li>
+                <li>
+                  <a href="#">TV</a>
+                </li>
+                <li>
+                  <a href="#">Games</a>
+                </li>
+                <li>
+                  <a href="#">Videos</a>
+                </li>
+                <li>
+                  <a href="#">News</a>
+                </li>
+              </ul>
+              <h3 className="second-title">SHOP</h3>
+              <ul>
+                <li>
+                  <a href="#">Shop DC</a>
+                </li>
+                <li>
+                  <a href="#">Shop DC Collectibles</a>
+                </li>
+              </ul>
+            </div>
+            <div className="second-ul">
+              <h3>DC</h3>
+              <ul>
+                <li>
+                  <a href="#">Terms Of Use</a>
+                </li>
+                <li>
+                  <a href="#">Privacy Policy (New)</a>
+                </li>
+                <li>
+                  <a href="#">Ad Choices</a>
+                </li>
+                <li>
+                  <a href="#">Advertising</a>
+                </li>
+                <li>
+                  <a href="#">Jobs</a>
+                </li>
+                <li>
+                  <a href="#">Subscriptions</a>
+                </li>
+                <li>
+                  <a href="#">Talent Workshops</a>
+                </li>
+                <li>
+                  <a href="#">CPSC Certificates</a>
+                </li>
+                <li>
+                  <a href="#">Ratings</a>
+                </li>
+                <li>
+                  <a href="#">Shop Help</a>
+                </li>
+                <li>
+                  <a href="#">Contact Us</a>
+                </li>
+              </ul>
+            </div>
+            <div className="third-ul">
+            <h3 className="third-title">SITES</h3>
+              <ul>
+                <li>
+                  <a href="#">DC</a>
+                </li>
+                <li>
+                  <a href="#">MAD Magazine</a>
+                </li>
+                <li>
+                  <a href="#">DC Kids</a>
+                </li>
+                <li>
+                  <a href="#">DC Universe</a>
+                </li>
+                <li>
+                  <a href="#">DC Power Visa</a>
+                </li>
+              </ul>
+            </div>
+            <div className="img-footer-container">
+              <img src="./img/dc-logo-bg.png" alt=""/>
+            </div>
+          </div>
+        </div>
+        <div className="last-footer">
+        <div className="container-last-footer">
+            <div className="left-footer">
+                <button className="sign-up">SIGN-UP NOW!</button>
+            </div>
+            <div className="right-footer">
+                <h3>FOLLOW US</h3>
+                <div className="img-last-footer">
+                    <img src="./img/footer-facebook.png" alt="" />
+                    <img src="./img/footer-twitter.png" alt="" />
+                    <img src="./img/footer-youtube.png" alt="" />
+                    <img src="./img/footer-pinterest.png" alt="" />
+                    <img src="./img/footer-periscope.png" alt="" />
+                </div>
+            </div>
+        </div>
+      </div>
+    </footer>
+  );
 }
--- src/assets/components/Header.jsx
+++ src/assets/components/Header.jsx
@@ -1,7 +1,42 @@
 export default function Header () {
     return (
-        <header>
-           <h1>ciao</h1>
-        </header>
-    )
+        <div className="container">
+            <img className="logo-img" src="./img/dc-logo.png" alt="logo"></img>
+            <nav className="nav-header">
+                <ul>
+                    <li>
+                        <a href="#">CHARACTERS</a>
+                    </li>
+                    <li>
+                        <a id="comics" href="#">COMICS</a>
+                        <hr className="cazzillo"></hr>    
+                    </li>
+                    <li>
+                        <a href="#">MOVIES</a>
+                    </li>
+                    <li>
+                        <a href="#">TV</a>
+                    </li>
+                    <li>
+                        <a href="#">GAMES</a>
+                    </li>
+                    <li>
+                        <a href="#">COLLECTIBLES</a>
+                    </li>
+                    <li>
+                        <a href="#">VIDEOS</a>
+                    </li>
+                    <li>
+                        <a href="#">FANS</a>
+                    </li>
+                    <li>
+                        <a href="#">NEWS</a>
+                    </li>
+                    <li>
+                        <a href="#">SHOP</a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    )      
 }
